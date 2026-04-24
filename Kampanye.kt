76b3356cbda1e6147694a86ef934c437d41eeb5e
Haekal <0431053@student.itk.ac.id>
2026-04-24 21:56:27 +0800

    Inisialisasi project dan menambah class entitas dasar

--- Kampanye.kt
+++ Kampanye.kt
@@ -1,4 +1,19 @@
 class KampanyeDonasi(val judul: String, val target: Double) {
     var danaTerkumpul: Double = 0.0
         private set
+
+    fun terimaDonasi(nominal: Double): Boolean {
+        if (danaTerkumpul + nominal > target) {
+            println("❌ Gagal: donasi melebihi target kampanye")
+            return false
+        }
+
+        danaTerkumpul += nominal
+        println("Total dana sekarang: Rp$danaTerkumpul")
+        return true
+    }
+
+    fun sisaTarget(): Double {
+        return target - danaTerkumpul
+    }
 }
