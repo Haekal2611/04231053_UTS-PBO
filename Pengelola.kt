442e81ae984e08654f11949a8b00d08e5eb2731c
Haekal <0431053@student.itk.ac.id>
2026-04-24 21:58:06 +0800

    Inisialisasi project dan menambah class entitas dasar

--- Pengelola.kt
+++ Pengelola.kt
@@ -1,1 +1,10 @@
-class Pengelola(val nama: String)
+class Pengelola(val nama: String) {
+    fun lihatStatus(kampanye: KampanyeDonasi) {
+        println("\n=== STATUS KAMPANYE ===")
+        println("Pengelola: $nama")
+        println("Judul: ${kampanye.judul}")
+        println("Dana terkumpul: Rp${kampanye.danaTerkumpul}")
+        println("Target: Rp${kampanye.target}")
+        println("Sisa target: Rp${kampanye.sisaTarget()}")
+    }
+}
