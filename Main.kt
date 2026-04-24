997337e496cb5582f8d47c54c7a4f02db9260391
Haekal <0431053@student.itk.ac.id>
2026-04-24 21:41:09 +0800

    Inisialisasi project dan menambah class entitas dasar

--- Main.kt
+++ Main.kt
@@ -1,4 +1,17 @@
 fun main() {
-    val donatur = Donatur("Haekal", 500000.0)
-    donatur.donasi(200000.0)
+    val kampanye = KampanyeDonasi("Galang Dana Mahasiswa", 1000000.0)
+
+    val haekal = Donatur("Haekal", 500000.0)
+    val rahmad = Donatur("Rahmad", 100000.0)
+
+    val pengelola = Pengelola("Rubian")
+
+    println("=== SIMULASI DONASI ===")
+
+    haekal.donasi(kampanye, 200000.0)
+    rahmad.donasi(kampanye, 150000.0)
+    haekal.donasi(kampanye, 300000.0)
+    rahmad.donasi(kampanye, 100000.0)
+
+    pengelola.lihatStatus(kampanye)
 }
